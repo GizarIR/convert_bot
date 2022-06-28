e68111f0b3cea059ee4f1a7792943a616285c158
Gizar <gizarir@gmail.com>
2022-06-28 18:54:15 +0300

    Add conversation with bot

--- app.py
+++ app.py
@@ -20,6 +20,35 @@ def values(message: telebot.types.Message):
         text = "\n".join([text, key])
     bot.reply_to(message, text)
 
+# реализация диалога с ботом
+@bot.message_handler(commands=['convert'])
+def values(message: telebot.types.Message):
+    text = "Выберите валюту, из которой хотите конвертировать"
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, quote_handler)
+
+def quote_handler(message: telebot.types.Message):
+    quote = message.text.strip()
+    text = "Выберите валюту, в которую Вы хотите конвертировать"
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, base_handler, quote)
+
+def base_handler(message: telebot.types.Message, quote):
+    base = message.text.strip()
+    text = "Выберите сумму конвертации"
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, amount_handler, quote, base)
+
+def amount_handler(message: telebot.types.Message, quote, base):
+    amount = message.text.strip()
+    try:
+        total_base = CryptoConvert.get_price(quote, base, amount)
+    except ConvertException as e:
+        bot.send_message(message.chat.id, f'Ошибка конвертации. \n{e}')
+    else:
+        text = f'{amount} {quote}  это  {total_base} {base}'
+        bot.send_message(message.chat.id, text)
+
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
