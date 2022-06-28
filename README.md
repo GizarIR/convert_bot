76e3b630d3a4ba54d7b990d742ecd61c630f84f1
Gizar <gizarir@gmail.com>
2022-06-28 17:04:34 +0300

    Release 001

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # convert_bot is app for currency conversion
+
+# The bot returns the price for a certain amount of currency (euro, dollar or ruble).
+# A person should send a message to the bot in the form of <name1> <name2> <quantity>: 
+#     name1 - <name of the currency whose price he wants to find out> 
+#     name2 -  <name of the currency, in which to find out the price of the first currency> 
+#     quantity  - <quantity of the first currency>.
+# When entering the /start or /help command, the user is shown instructions for using the bot.
+# When entering the /values command, information about all available currencies should be displayed 
+# in a readable form.
--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 import telebot
+from extensions import ConvertException, CryptoConvert
+from config import TOKEN, keys
 
-TOKEN ='5468058144:AAEKHdTA40eeou88cMCsMblvB4b2pdiz21w'
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, f"Hello {message.chat.username}")
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Для начала работы бота введите команду в формате: ' \
+'<имя валюты> <имя валюты, в которую надо перевести> <количество первой валюты>. ' \
+'Чтобы узнать доступные валюты введите команду:  /values'
+    bot.reply_to(message, text)
 
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты:"
+    for key in keys.keys():
+        text = "\n".join([text, key])
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertException('Количество введенных параметров не совпадает с требуемым - 3 параметра')
+
+        quote, base, amount = values
+
+        total_base = CryptoConvert.get_price(quote.lower(), base.lower(), amount)
+    except ConvertException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
+    else:
+        text = f'{amount} {quote}  это  {total_base} {base}'
+        bot.send_message(message.chat.id, text)
 
 bot.polling()
